2dd90f7c1727377570617b1f3de3d3f29e3d9085
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-01-27 10:15:28 +0100

    Setup of project

---
+++ src/main/kotlin/no/nav/k9/azuremock/AzureMock.kt
@@ -0,0 +1,170 @@
+package no.nav.k9.azuremock
+
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.origin
+import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.*
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.getOrFail
+import kotlinx.html.*
+import no.nav.helse.dusseldorf.testsupport.http.AzureToken
+import no.nav.helse.dusseldorf.testsupport.http.AzureWellKnown
+import no.nav.helse.dusseldorf.testsupport.http.TokenRequest
+import no.nav.helse.dusseldorf.testsupport.jws.Azure
+import java.net.URLDecoder
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+private object Konstanter {
+    internal const val basePath = "/v2.0"
+    internal const val wellKnownPath = "$basePath/.well-known/openid-configuration"
+    internal const val tokenPath = "$basePath/token"
+    internal const val authorizationPath = "$basePath/authorize"
+    internal const val jwksPath = "$basePath/jwks"
+}
+
+@KtorExperimentalAPI
+fun Application.AzureMock() {
+    install(Routing) {
+        get(Konstanter.wellKnownPath) {
+            val baseUrl = call.request.baseUrl()
+            val wellKnownResponse = AzureWellKnown.response(
+                    issuer = call.request.issuer(),
+                    tokenEndpoint = "$baseUrl${Konstanter.tokenPath}",
+                    authorizationEndpoint = "$baseUrl${Konstanter.authorizationPath}",
+                    jwksUri = "$baseUrl${Konstanter.jwksPath}"
+            )
+            call.respondText(
+                    contentType = ContentType.Application.Json,
+                    status = HttpStatusCode.OK,
+                    text = wellKnownResponse
+            )
+        }
+
+        get(Konstanter.jwksPath) {
+            call.respondText(
+                    contentType = ContentType.Application.Json,
+                    status = HttpStatusCode.OK,
+                    text = Azure.V2_0.getPublicJwk()
+            )
+        }
+
+        post(Konstanter.tokenPath) {
+            val tokenResponse = AzureToken.response(
+                    request = KtorTokenRequest(
+                            call = call,
+                            body = call.receiveText()
+                    ),
+                    issuer = call.request.issuer()
+            )
+            call.respondText(
+                    contentType = ContentType.Application.Json,
+                    status = HttpStatusCode.OK,
+                    text = tokenResponse
+            )
+        }
+
+        get(Konstanter.authorizationPath) {
+            val clientId = call.request.queryParameters.getOrFail("client_id")
+            val redirectUri = call.request.queryParameters.getOrFail("redirect_uri")
+            val scope = call.request.queryParameters.getOrFail("scope")
+            val state = call.request.queryParameters.getOrFail("state")
+            val nonce = call.request.queryParameters.getOrFail("nonce")
+
+            call.respondHtml {
+                head {
+                    title { +"Azure Mock Login Input" }
+                }
+                body {
+                    form(action = Konstanter.authorizationPath,
+                         encType = FormEncType.applicationXWwwFormUrlEncoded,
+                         method = FormMethod.post) {
+                        acceptCharset = "utf-8"
+                        h1 { +"Azure Mock Login Input"}
+                        p {
+                            label { +"Bruker ID: " }
+                            textInput(name = "user_id")
+                        }
+                        p {
+                            label { +"Navn: " }
+                            textInput(name = "name")
+                        }
+                        hiddenInput(name = "client_id") { value = clientId }
+                        hiddenInput(name = "scope") { value = scope }
+                        hiddenInput(name = "state") { value = state }
+                        hiddenInput(name = "nonce") { value = nonce }
+                        hiddenInput(name = "redirect_uri") { value = redirectUri }
+                        submitInput{ value = "Fortsett"}
+                    }
+                }
+            }
+        }
+
+        post(Konstanter.authorizationPath) {
+            val parameters = call.receiveParameters()
+            val userId = parameters.getOrFail("user_id")
+            val name = parameters["name"] ?: userId
+            val clientId = parameters.getOrFail("client_id")
+            val scope = parameters.getOrFail("scope")
+            val state = parameters.getOrFail("state")
+            val nonce = parameters.getOrFail("nonce")
+            val redirectUri = parameters.getOrFail("redirect_uri")
+
+            val code = AzureToken.Code(
+                    userId = userId,
+                    name = name,
+                    scopes = scope,
+                    nonce = nonce
+            )
+
+            call.respondHtml {
+                head {
+                    title { +"Azure Mock Login Oppsummering" }
+                }
+                body {
+                    form(action = redirectUri,
+                         encType = FormEncType.applicationXWwwFormUrlEncoded,
+                         method = FormMethod.post) {
+                        acceptCharset = "utf-8"
+                        h1 { +"Azure Mock Login Oppsummering"}
+                        p { +"Bruker ID: $userId"}
+                        p { +"Navn: $name"}
+                        p { +"Client ID: $clientId"}
+                        p { +"Scope: $scope"}
+                        p { +"State: $state"}
+                        p { +"Nonce: $nonce"}
+                        p { +"Redirect URI: $redirectUri"}
+
+                        hiddenInput(name = "code") { value = code.toString() }
+                        hiddenInput(name = "state") { value = state }
+                        submitInput {value ="Bekreft"}
+                    }
+                }
+            }
+        }
+    }
+}
+
+private class KtorTokenRequest(
+        call: ApplicationCall,
+        body: String
+): TokenRequest {
+    private val urlDecodedBody = URLDecoder.decode(body, Charsets.UTF_8)
+    private val authorizationHeader = call.request.header(HttpHeaders.Authorization)
+    override fun urlDecodedBody(): String = urlDecodedBody
+    override fun authorizationHeader() = authorizationHeader
+
+}
+
+private fun ApplicationRequest.baseUrl() = "${origin.scheme}://${host()}:${port()}"
+private fun ApplicationRequest.issuer() = "${baseUrl()}${Konstanter.basePath}"
---
+++ src/test/kotlin/no/nav/k9/azuremock/AzureMockTest.kt
@@ -0,0 +1,91 @@
+package no.nav.k9.azuremock
+
+/*  SELF
+Access Token:
+{
+  "aud": "3ccdda02-eaf5-4c75-af01-ea0ee7d01819",
+  "iss": "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0",
+  "iat": 1579891793,
+  "nbf": 1579891793,
+  "exp": 1579895693,
+  "aio": "AWQAm/8OAAAAd1XbPt+f8uA3QSPqVW0lG6mpJFg5ZF7AknQSHI+pwfgdJCDzvuMBSKRvbm4ry9INhmF8iOron6+LlrSwmrBh4HBoF9yp9JV5S2F6T6lJ3xsfF6RBqH0se212ML4B7EIw",
+  "azp": "3ccdda02-eaf5-4c75-af01-ea0ee7d01819",
+  "azpacr": "2",
+  "groups": [
+    "841e0106-81a1-4860-bc46-b4e17e125b1b",
+    "01dce2aa-b36a-480c-a7fb-e0cbef56ece2",
+    "ceab8bf0-0771-4478-bc39-186629b8cc2b",
+    "928636f4-fd0d-4149-978e-a6fb68bb19de",
+    "cd3983f1-fbc1-4c33-9f31-f7a40e422ccd",
+    "2e98e6dd-fc7f-46b2-b96f-3baae4907458",
+    "09880b8b-48d7-4048-b2ea-dff7be97e193"
+  ],
+  "name": "Erik Maximilian Forsman",
+  "oid": "7e4febc0-23e4-4764-8f9c-9e57676994e8",
+  "preferred_username": "Erik.Maximilian.Forsman@trygdeetaten.no",
+  "scp": "User.Read",
+  "sub": "VlWcY8YiOg84OqIhkGhMGIuheTpiI8_5Q_Qph-uBob4",
+  "tid": "966ac572-f5b7-4bbe-aa88-c76419c0f851",
+  "uti": "a8FVjIMlgkqaVu-h6a4rAA",
+  "ver": "2.0"
+}
+
+ID-Token:
+
+{
+  "aud": "3ccdda02-eaf5-4c75-af01-ea0ee7d01819",
+  "iss": "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0",
+  "iat": 1579891793,
+  "nbf": 1579891793,
+  "exp": 1579895693,
+  "groups": [
+    "841e0106-81a1-4860-bc46-b4e17e125b1b",
+    "01dce2aa-b36a-480c-a7fb-e0cbef56ece2",
+    "ceab8bf0-0771-4478-bc39-186629b8cc2b",
+    "928636f4-fd0d-4149-978e-a6fb68bb19de",
+    "cd3983f1-fbc1-4c33-9f31-f7a40e422ccd",
+    "2e98e6dd-fc7f-46b2-b96f-3baae4907458",
+    "09880b8b-48d7-4048-b2ea-dff7be97e193"
+  ],
+  "name": "Erik Maximilian Forsman",
+  "nonce": "snFpwHCG_vpdUFIYNOtDEhzJ33gvB1tEd0u6UdHIvGo",
+  "oid": "7e4febc0-23e4-4764-8f9c-9e57676994e8",
+  "preferred_username": "Erik.Maximilian.Forsman@trygdeetaten.no",
+  "sub": "VlWcY8YiOg84OqIhkGhMGIuheTpiI8_5Q_Qph-uBob4",
+  "tid": "966ac572-f5b7-4bbe-aa88-c76419c0f851",
+  "uti": "a8FVjIMlgkqaVu-h6a4rAA",
+  "ver": "2.0"
+}
+
+ON BEHALF OF:
+Access token:
+{
+  "aud": "c7adbfbb-1b1e-41f6-9b7a-af9627c04998",
+  "iss": "https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/v2.0",
+  "iat": 1579891840,
+  "nbf": 1579891840,
+  "exp": 1579895692,
+  "aio": "AWQAm/8OAAAAujk3JmCiowdozC6Q+/B9Awvg/XPRzJyvSGEtmwzdyXLJntyrnbbqs8X/kee5+09VW9Ghvq4Ka5mDxuv7z2AHhK3puGTm426Kbfy3VDotka1EEe92qNc0T3QKW08yLyuK",
+  "azp": "3ccdda02-eaf5-4c75-af01-ea0ee7d01819",
+  "azpacr": "2",
+  "groups": [
+    "841e0106-81a1-4860-bc46-b4e17e125b1b",
+    "01dce2aa-b36a-480c-a7fb-e0cbef56ece2",
+    "ceab8bf0-0771-4478-bc39-186629b8cc2b",
+    "928636f4-fd0d-4149-978e-a6fb68bb19de",
+    "cd3983f1-fbc1-4c33-9f31-f7a40e422ccd",
+    "2e98e6dd-fc7f-46b2-b96f-3baae4907458",
+    "09880b8b-48d7-4048-b2ea-dff7be97e193"
+  ],
+  "name": "Erik Maximilian Forsman",
+  "oid": "7e4febc0-23e4-4764-8f9c-9e57676994e8",
+  "preferred_username": "Erik.Maximilian.Forsman@trygdeetaten.no",
+  "scp": "defaultaccess",
+  "sub": "ycsxWK6P7PYW7kcq4esnGdIzUfDt1pndlEPUcmnLjtA",
+  "tid": "966ac572-f5b7-4bbe-aa88-c76419c0f851",
+  "uti": "cRPV341Af0-fEP2QZ00uAA",
+  "ver": "2.0"
+}
+
+
+ */
